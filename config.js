07a1d2924b0e064e584b3100300fa5052be0a359
AayanBinBamasaq <78581645+AayanBinBamasaq@users.noreply.github.com>
2022-01-21 00:56:25 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCiljgjnV1PABwZn2DlbGqzOyU7hErAeF4",
+    authDomain: "ayaanc93-1f61d.firebaseapp.com",
+    projectId: "ayaanc93-1f61d",
+    storageBucket: "ayaanc93-1f61d.appspot.com",
+    messagingSenderId: "556107805608",
+    appId: "1:556107805608:web:5dff79e0eeb5252a21e39d"
+  };
+  export default firebaseConfig;
